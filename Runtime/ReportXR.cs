178a52950066250c0c987c60fb7ed8ce2e9b9a83
rodriarce <56464455+rodriarce@users.noreply.github.com>
2025-06-19 14:15:47 -0300

    wip movimientos de carpetas

--- Runtime/ReportXR.cs
+++ Runtime/ReportXR.cs
@@ -1,11 +1,16 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 
 namespace SingularisVR.ReportXR
 {
-    public class ReportXR : MonoBehaviour
+    public class ReportXR 
     {
         private static bool isInitialized = false;
+        private static AndroidJavaClass unityClass;
+        private static AndroidJavaObject unityActivity;
+        private static AndroidJavaObject rXRLib;
+        
 
         /// <summary>
         /// Inicializa el SDK de informXR, envolviendo la inicialización en tu propio SDK.
@@ -33,11 +38,13 @@ namespace SingularisVR.ReportXR
             Event(name, null);
         }
 
+      
+
         public static void Event(string name, Dictionary<string, string> meta = null)
         {
-            EnsureInitialized();
+            //EnsureInitialized();
 
-            InformXRManager.TrackEvent(name, meta); // Llama al método real del SDK base
+            //InformXRManager.TrackEvent(name, meta); // Llama al método real del SDK base
             Debug.Log($"Evento enviado: {name}");
         }
 
@@ -53,14 +60,14 @@ namespace SingularisVR.ReportXR
         {
 
 
-            InformXRManager.TrackLevelStart(levelName, meta); // Llama al SDK base
+            //InformXRManager.TrackLevelStart(levelName, meta); // Llama al SDK base
             Debug.Log($"Nivel iniciado: {levelName}");
         }
 
         public static void EventLevelStart(string levelName, string metaString = "")
         {
-            var meta = ParseMetaString(metaString);
-            EventLevelStart(levelName, meta);
+            //var meta = ParseMetaString(metaString);
+            //EventLevelStart(levelName, meta);
         }
 
         // --------------------------
@@ -73,19 +80,53 @@ namespace SingularisVR.ReportXR
 
         public static void EventLevelComplete(string levelName, int score, Dictionary<string, string> meta = null)
         {
-            EnsureInitialized();
+            //EnsureInitialized();
 
 
         }
 
         public static void EventLevelComplete(string levelName, int score, string metaString = "")
         {
-            var meta = ParseMetaString(metaString);
-            EventLevelComplete(levelName, score, meta);
+            //var meta = ParseMetaString(metaString);
+            //EventLevelComplete(levelName, score, meta);
         }
 
+
+        public static void CallAddEvent()
+        {
+
+            unityClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+            unityActivity = unityClass.GetStatic<AndroidJavaObject>("currentActivity");
+
+            rXRLib = new AndroidJavaObject("com.singularisvr.reportxr.plugin.rXRLib");
+
+            try
+            {
+                string version = rXRLib.CallStatic<string>("getVersion");
+                Debug.Log("ReportXR Version: " + version);
+            }
+            catch (AndroidJavaException e)
+            {
+                Debug.LogError("Failed: string getVersion(): " + e.Message);
+            }         
+
+            try
+            {
+                bool success = rXRLib.CallStatic<bool>("sendEvent", unityActivity, 1, 1, 1, "unity", "sample");
+                Debug.Log("sendEvent success: " + success);
+            }
+            catch (AndroidJavaException e)
+            {
+                Debug.LogError("Failed: bool sendEvent(Activity activity, int eventType, int eventSubType, int eventValue, String eventSource, String eventData): " + e.Message);
+            }
+
+
+        }
     }
 
+
 }
+
+
 
 
