99e8656cb64146b398323fc3c76780fec36d8520
Felipe Jimenez <felipe.jimenez@tdcla.com>
2025-07-24 11:18:53 -0500

    send track

--- Runtime/UnityCallbackProxy.cs
+++ Runtime/UnityCallbackProxy.cs
@@ -1,22 +1,18 @@
 using System;
 using UnityEngine;
 
-namespace SingularisVR.ReportXR
-{
-    public class UnityCallbackProxy : AndroidJavaProxy
-    {
+namespace SingularisVR.ReportXR {
+    public class UnityCallbackProxy : AndroidJavaProxy {
 
         public Action<string> onResult;
 
 
-        public UnityCallbackProxy(Action<string> onResult) : base("com.singularisvr.reportxr.plugin.UnityCallback")
-        {
+        public UnityCallbackProxy(Action<string> onResult) : base("com.singularisvr.reportxr.plugin.UnityCallback") {
             this.onResult = onResult;
         }
 
 
-        public void sendResult(string result)
-        {
+        public void sendResult(string result) {
             if (onResult != null)
                 onResult(result);
             else
